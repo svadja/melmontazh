4f2d4d6683060af7cd345cb3f26c516fdc2570f7
svadja <svadja@rambler.ru>
2014-05-21 16:02:09 +0300

    Change-Id: I074c0bcfa51e6f4c9c798b47af0bd5aaa75f6eb9

--- src/main/java/com/pp/melmontazh/domain/Image.java
+++ src/main/java/com/pp/melmontazh/domain/Image.java
@@ -12,7 +12,7 @@ public class Image {
     @Id
     @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private long id;
-    private String discription;
+    private String name;
     private String source;
 
     public long getId() {
@@ -21,11 +21,11 @@ public class Image {
 	public void setId(long id) {
 		this.id = id;
 	}
-	public String getDiscription() {
-		return discription;
+	public String getName() {
+		return name;
 	}
-	public void setDiscription(String discription) {
-		this.discription = discription;
+	public void setName(String name) {
+		this.name = name;
 	}
 	public String getSource() {
 		return source;
