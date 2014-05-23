52ef93242ae4ff7539c0cbe2e35f6e06ba92ccf3
svadja <svadja@rambler.ru>
2014-05-23 17:00:48 +0300

    Change-Id: I381077f445a4fe7bd205dad2934ec237b8df3432

--- src/main/java/com/pp/melmontazh/domain/Service.java
+++ src/main/java/com/pp/melmontazh/domain/Service.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+ 
 @Entity
 @Table(name="Services")
 public class Service {
